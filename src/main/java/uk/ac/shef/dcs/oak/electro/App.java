99cf34b0943117cba57f7009f20bd73443005ed2
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2012-02-08 13:54:27 +0000

    First Pass

---
+++ src/main/java/uk/ac/shef/dcs/oak/electro/App.java
@@ -0,0 +1,13 @@
+package uk.ac.shef.dcs.oak.electro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
