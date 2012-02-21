6d2ccec0db2b80ccbe53c9bc1a0712167f2a097e
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2012-02-21 12:08:20 +0000

    Fixed the graph panel

--- src/main/java/uk/ac/shef/dcs/oak/electro/GraphPanel.java
+++ src/main/java/uk/ac/shef/dcs/oak/electro/GraphPanel.java
@@ -8,6 +8,19 @@ import javax.swing.JPanel;
 
 public class GraphPanel extends JPanel implements ModelListener
 {
+   public static void main(String[] args) throws Exception
+   {
+      JFrame framer = new JFrame();
+      GraphPanel mine = new GraphPanel(new Model(0, 60 * 60));
+      framer.add(mine);
+
+      framer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      framer.setSize(500, 500);
+      framer.setLocationRelativeTo(null);
+      framer.setVisible(true);
+
+   }
+
    Model mod;
 
    public GraphPanel(Model mod)
@@ -48,18 +61,5 @@ public class GraphPanel extends JPanel implements ModelListener
 
       g.setColor(Color.red);
       g.drawLine(this.getWidth() - 1, 0, this.getWidth() - 1, this.getHeight());
-   }
-
-   public static void main(String[] args) throws Exception
-   {
-      JFrame framer = new JFrame();
-      GraphPanel mine = new GraphPanel(new Model(0, 60 * 60 * 24));
-      framer.add(mine);
-
-      framer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      framer.setSize(500, 500);
-      framer.setLocationRelativeTo(null);
-      framer.setVisible(true);
-
    }
 }
